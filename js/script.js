0d8fbe2fdf95c3625d9d8466aa87082dd6905e33
ooPeachBoy <1774233612@qq.com>
2019-09-08 23:02:54 +0800

    Site updated: 2019-09-08 23:02:51

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ document.ready(
     function () {
         var _Blog = window._Blog || {};
         const currentTheme = window.localStorage && window.localStorage.getItem('theme');
-        const isDark = currentTheme === 'dark';
+        const isDark = currentTheme !== 'Dark';
         if (isDark) {
             document.getElementById("switch_default").checked = true;
             // mobile
